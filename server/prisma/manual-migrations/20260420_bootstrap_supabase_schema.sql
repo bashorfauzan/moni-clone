5ec3cb5771d57ba97bcfaca6fb35c652d8a26148
Bashor Fauzan <bashorfauzan@users.noreply.github.com>
2026-05-31 05:29:55 +0700

    Restore immutable bootstrap migration checksum

--- server/prisma/manual-migrations/20260420_bootstrap_supabase_schema.sql
+++ server/prisma/manual-migrations/20260420_bootstrap_supabase_schema.sql
@@ -96,7 +96,6 @@ CREATE TABLE "Budget" (
 CREATE TABLE "Target" (
     "id" TEXT NOT NULL,
     "title" TEXT NOT NULL,
-    "notes" TEXT,
     "totalAmount" DOUBLE PRECISION NOT NULL,
     "remainingAmount" DOUBLE PRECISION NOT NULL,
     "remainingMonths" INTEGER NOT NULL,
